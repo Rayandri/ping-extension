1d5bf823c5758bcab0870f1da6a927444b7ad230
RayanDri <rayan.drissi@icloud.com>
2024-06-25 15:23:21 +0200

    chore: Add status bar and commands for code compilation, committing, and function creation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
+let points = 0;
+let statusBar: vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
+  statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  statusBar.text = `Points: ${points}`;
+  statusBar.show();
+  
+  context.subscriptions.push(statusBar);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ping-gamefi" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ping-gamefi.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Ping-GameFi!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(vscode.commands.registerCommand('extension.compileCode', compileCode));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.commitCode', commitCode));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.createFunction', createFunction));
 }
 
-// This method is called when your extension is deactivated
+function compileCode() {
+  points += 10;
+  updateStatusBar();
+  vscode.window.showInformationMessage(`Code compiled! Total points: ${points}`);
+}
+
+function commitCode() {
+  points += 5;
+  updateStatusBar();
+  vscode.window.showInformationMessage(`Code committed! Total points: ${points}`);
+}
+
+function createFunction() {
+  points += 1;
+  updateStatusBar();
+  vscode.window.showInformationMessage(`Function created! Total points: ${points}`);
+}
+
+function updateStatusBar() {
+  statusBar.text = `Points: ${points}`;
+}
+
 export function deactivate() {}
